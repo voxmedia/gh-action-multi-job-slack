b8be730947b2eaaaf63cafc0b8d5bed71d59701b
Jason Ormand <jason.ormand1@gmail.com>
2020-02-26 16:29:35 -0700

    dump tons of data

--- src/init.js
+++ src/init.js
@@ -1,11 +1,52 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+var fs = require('fs');
 
 function init(){
   console.log("Initing!")
-  // Get workflow context/file
-    // create state for each step
-    // post message to slack
+
+  const repo_path = process.env.GITHUB_REPOSITORY
+  const owner = repo_path.split('/')[0]
+  const repo = repo_path.split('/')[1]
+  const workflow = process.env.GITHUB_WORKFLOW
+  const run_id = process.env.GITHUB_RUN_ID
+  const run_number = process.env.GITHUB_RUN_NUMBER
+  const actor = process.env.GITHUB_ACTOR
+  const event_name = process.env.GITHUB_EVENT_NAME
+  const event_path = process.env.GITHUB_EVENT_PATH
+  console.log({
+    repo: repo,
+    workflow: workflow,
+    run_id: run_id,
+    run_number: run_number,
+    actor: actor,
+    event_name: event_name,
+    event_path: event_path
+  })
+
+  const { data: octo_workflow } = octokit.actions.getWorkflow({
+    owner,
+    repo,
+    workflow_id: workflow
+  });
+  console.log({ octo_workflow });
+
+  const { data: octo_workflow_run } = octokit.actions.getWorkflowRun({
+    owner,
+    repo,
+    run_id
+  });
+  console.log({ octo_workflow_run });
+
+  const { data: octo_jobs } = octokit.actions.listJobsForWorkflowRun({
+    owner,
+    repo,
+    run_id
+  });
+  console.log({ octo_jobs });
+
+  var event = JSON.parse(fs.readFileSync(event_path, 'utf8'));
+  console.log({ event })
 }
 
 async function run() {
